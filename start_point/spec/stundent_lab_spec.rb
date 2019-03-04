bc689fc1385fb2f36c406245cf10c58062a5b788
Myriam Boran <brnmyriam45@googlemail.com>
2019-03-04 21:11:18 +0000

    forgot indentation

--- start_point/spec/stundent_lab_spec.rb
+++ start_point/spec/stundent_lab_spec.rb
@@ -4,40 +4,35 @@ require_relative('../student_lab')
 
 class TestStudent < MiniTest::Test
 
-def test_student_name
-  student = Student.new('Myriam', 'E29', 'ruby')
-  assert_equal('Myriam', student.name)
-end
+  def test_student_name
+    student = Student.new('Myriam', 'E29', 'ruby')
+    assert_equal('Myriam', student.name)
+  end
 
-def test_student_cohort
-  student = Student.new('Pim', 'G21', 'java')
-  assert_equal('G21', student.cohort)
-end
+  def test_student_cohort
+    student = Student.new('Pim', 'G21', 'java')
+    assert_equal('G21', student.cohort)
+  end
 
-def test_update_student_name
-  student = Student.new('Myriam', 'E29', 'ruby')
-  student.name = 'Carol'
-  assert_equal('Carol', student.name)
-end
+  def test_update_student_name
+    student = Student.new('Myriam', 'E29', 'ruby')
+    student.name = 'Carol'
+    assert_equal('Carol', student.name)
+  end
 
-def test_update_student_cohort
-  student = Student.new('Pim', 'G21', 'java')
-  student.cohort = 'G5'
-  assert_equal('G5', student.cohort)
-end
+  def test_update_student_cohort
+    student = Student.new('Pim', 'G21', 'java')
+    student.cohort = 'G5'
+    assert_equal('G5', student.cohort)
+  end
 
-def test_student_talks
-  student = Student.new('Jane','G29', 'JavaScript')
-  assert_equal("I can talk", student.student_talks)
-end
+  def test_student_talks
+    student = Student.new('Jane','G29', 'JavaScript')
+    assert_equal("I can talk", student.student_talks)
+  end
 
-def test_favourite_language
-  student = Student.new('Lidia', 'C12', 'python')
-  assert_equal('I love python', student.favourite_language)
-end
-
-
-
-
-
+  def test_favourite_language
+    student = Student.new('Lidia', 'C12', 'python')
+    assert_equal('I love python', student.favourite_language)
+  end
 end
